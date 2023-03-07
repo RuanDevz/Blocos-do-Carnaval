38acb64a40d52ee46b4cdf9cef03f3ceb1d3f465
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-06 22:16:01 -0300

    botao pronto

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-lista.addEventListener('click', () => {
-  console.log('lista foi clicada!');
-  // Verificar se o estilo de fundo do botão "Lista" é definido inline
-  if (lista.style.backgroundColor !== '') {
-    // Copiar as propriedades de estilo do botão "Lista" para o botão "Mapa"
-    mapa.style.backgroundColor = lista.style.backgroundColor;
-    mapa.style.color = lista.style.color;
-  }
-});
+const lista = document.getElementById('lista');
+const mapa = document.getElementById('mapa');
+
+lista.addEventListener('click', btn1);
+mapa.addEventListener('click', btn2);
+
+lista.style.transition = 'background-color 0.3s ease, color 0.3s ease';
+mapa.style.transition = 'background-color 0.3s ease, color 0.3s ease';
+
+function btn1() {
+  lista.style.background = '#6246ea';
+  lista.style.color = '#FFF';
+  mapa.style.background = '#FFF';
+  mapa.style.color = '#6246ea';
+}
+
+function btn2() {
+  lista.style.background = '#FFF';
+  lista.style.color = '#6246ea';
+  mapa.style.background = '#6246ea';
+  mapa.style.color = '#FFF';
+}
