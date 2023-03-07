eaf9b51f78bd7961351d18e16ce97d82500c956e
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-06 21:58:31 -0300

    simbora

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-const select = document.getElementById('select')
-const search = document.getElementById('search')
-const cards = document.querySelectorAll('.item')
-
-function filterCards() {
-  const selectedOption = select.value
-  const searchValue = search.value.toLowerCase()
-  cards.forEach(card => {
-    const cardOption = card.getAttribute('data-option')
-    const cardTitle = card.querySelector('h2').textContent.toLowerCase()
-    if ((selectedOption === 'todos' || cardOption === selectedOption) && cardTitle.includes(searchValue)) {
-      card.style.display = 'flex'
-    } else {
-      card.style.display = 'none'
-    }
-  })
-}
-
-select.addEventListener('change', filterCards)
-search.addEventListener('input', filterCards)
+lista.addEventListener('click', () => {
+  console.log('lista foi clicada!');
+  // Verificar se o estilo de fundo do botão "Lista" é definido inline
+  if (lista.style.backgroundColor !== '') {
+    // Copiar as propriedades de estilo do botão "Lista" para o botão "Mapa"
+    mapa.style.backgroundColor = lista.style.backgroundColor;
+    mapa.style.color = lista.style.color;
+  }
+});
